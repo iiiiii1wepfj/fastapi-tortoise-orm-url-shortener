6be363e41c5ab12bc06a42fb3a338f3d485a3e47
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-06-29 09:02:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# database url, here is the supported databases list: https://tortoise-orm.readthedocs.io/en/latest/databases.html
+database_url = "sqlite://linksdb.sqlite"
+# the port to run the app
+port = int("8000")
